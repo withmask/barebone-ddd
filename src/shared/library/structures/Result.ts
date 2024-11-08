63f49b4fa5344bef7cee931c36a0e01de395d808
withmask <with.mask@tutanota.com>
2024-11-08 16:39:37 +0000

    fix: Make result class errors more clear.

--- src/shared/library/structures/Result.ts
+++ src/shared/library/structures/Result.ts
@@ -1,85 +1,41 @@
-import type { Exception, TExceptionKind, TNarrow, TOmitNever } from 'shared';
+import type {
+  TNarrow,
+  Exception,
+  TExceptionKind
+} from 'shared';
 
 export type TVoidValue = (typeof Result)['_void'];
-export type TResult<T> = Result<true, T> | Result<false>;
+export type TResult<Type> = Result<Type>;
+
 export type TVoidResult = TResult<TVoidValue>;
 
-export class Result<
-  Ok extends boolean,
-  Data extends Ok extends false
-    ? Exception<TExceptionKind>
-    : any = Ok extends false ? Exception<TExceptionKind> : never
-> {
+export class Result<D>  {
   private static readonly _void: unique symbol = Symbol(
     'void_value:3d2ebe91-bdb6-4dd5-b02b-ecaff9b99948'
   );
   private _read: boolean;
 
   private constructor(
-    private readonly _ok: Ok,
-    private readonly _data: Data
+    private readonly _ok: boolean,
+    private readonly _value?: D,
+    private readonly _error?: Exception<TExceptionKind>
   ) {
     this._read = false;
   }
 
-  public static combined(...results: TResult<any>[]): TVoidResult {
-    for (const result of results) {
-      if (result.failed()) return result;
-    }
+  public static ok<T>(value: TNarrow<T>): Result<T> {
+    return new Result<T>(true, value as T);
+  }
 
-    return Result.done();
+  public static fail(error: Exception<TExceptionKind>): Result<never> {
+    return new Result<never>(false, undefined, error);
   }
 
   public static done(): TVoidResult {
-    return new Result(true, this._void as never);
+    return new Result(true, this._void);
   }
 
-  public static fail(exception: Exception<TExceptionKind>): Result<false> {
-    return new Result(false, exception);
-  }
-
-  public static ok<T>(
-    value: Exclude<TNarrow<T>, Exception<TExceptionKind>>
-  ): TResult<T> {
-    return new Result(true, value as T extends TVoidValue ? never : T);
-  }
-
-  public static read<P extends { [key: string]: TResult<any> }>(
-    results: P
-  ): TResult<
-    TOmitNever<{
-      [K in keyof P]: {
-        [T in P[K] as string]: T extends TVoidResult
-          ? never
-          : T extends TResult<infer V>
-            ? V
-            : never;
-      }[string];
-    }>
-  > {
-    const final: { [key: string]: any } = {};
-
-    for (const key in results) {
-      if (Object.prototype.hasOwnProperty.call(results, key)) {
-        if (results[key].failed()) return results[key];
-        if (results[key].void()) continue;
-        final[key as string] = results[key].value();
-      }
-    }
-
-    return Result.ok(final as any);
-  }
-
-  public error(): Ok extends false ? Data : never {
-    if (!this._read)
-      throw new Error('Cannot read result data before probing it.');
-
-    if (this._ok) throw new Error('Tried to get error of successful result.');
-
-    return this._data as Ok extends false ? Data : never;
-  }
-
-  public failed(): this is Result<false> {
+  public failed(): this is Result<never> {
     if (this._read && this._ok)
       throw new Error('Cannot probe successful result status twice.');
 
@@ -88,46 +44,39 @@ export class Result<
     return !this._ok;
   }
 
-  public lazy(): Exclude<Data, Exception<TExceptionKind>> {
-    if (this._ok) return this._data as any;
+  public error(): Exception<TExceptionKind> {
+    if (!this._read)
+      throw new Error('Cannot read result data before probing it.');
 
-    throw this._data;
+    if (this._ok) throw new Error('Tried to get error of successful result.');
+
+    return this._error!;
   }
 
-  public value(): Ok extends true
-    ? Data extends TVoidValue
-      ? never
-      : Data
-    : never {
+  public value(): D {
     if (!this._read)
       throw new Error('Cannot read result data before probing it.');
 
     if (!this._ok) throw new Error('Tried to get value of failed result.');
 
-    if (this._data === Result._void)
+    if (this._value === Result._void)
       throw new Error('This result carries no value.');
 
-    return this._data as Ok extends true
-      ? Data extends TVoidValue
-        ? never
-        : Data
-      : never;
+    return this._value!;
   }
 
-  public void(): Ok extends true
-    ? Data extends TVoidValue
-      ? true
-      : false
-    : never {
+  public void(): this is Result<TVoidValue> {
     if (!this._read)
       throw new Error('Cannot read result data before probing it.');
 
     if (!this._ok) throw new Error('Tried to get value of failed result.');
 
-    return (this._data === Result._void) as Ok extends true
-      ? Data extends TVoidValue
-        ? true
-        : false
-      : never;
+    return (this._value === Result._void) as boolean;
+  }
+
+  public lazy(): D {
+    if (this._ok) return this._value as any;
+
+    throw this._error;
   }
 }
